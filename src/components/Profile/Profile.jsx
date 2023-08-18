081091c57f0ccd55810ecb43a8d256230970d947
Teslyak <124871929+Teslyak@users.noreply.github.com>
2023-08-18 12:18:09 +0300

    hw1-react-profile

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,37 @@
+import {
+    ProfileWrap,
+    DescriptionWrap,
+
+} from "./Profile.styled"
+
+export const Profile = ({ userInfo: { avatar, username, tag, location, stats } }) => {
+    return (
+   <ProfileWrap>
+  <DescriptionWrap>
+    <img
+      src={avatar}
+      alt="User avatar"
+      className="avatar"
+    />
+    <p className="name">{username}</p>
+                <p className="tag">{tag}</p>
+    <p className="location">{location}</p>
+  </DescriptionWrap>
+
+  <ul className="stats">
+    <li>
+      <span className="label">Followers</span>
+      <span className="quantity">{stats.followers}</span>
+    </li>
+    <li>
+      <span className="label">Views</span>
+      <span className="quantity">{stats.views}</span>
+    </li>
+    <li>
+      <span className="label">Likes</span>
+      <span className="quantity">{stats.likes}</span>
+    </li>
+  </ul>
+            </ProfileWrap>
+    );
+}
---
+++ src/components/Profile/Profile.styled.jsx
@@ -0,0 +1,9 @@
+const { styled } = require('styled-components');
+
+export const ProfileWrap = styled.div`
+width: 100%;
+`;
+
+export const DescriptionWrap = styled.div`
+
+`;
